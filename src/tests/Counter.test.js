f788692ee9c21345d9f9fdcfff711cc8d4daf8fc
Stephen Smith <95247593+stephendsmith22@users.noreply.github.com>
2024-04-08 16:25:15 -0400

    Added unit tests for Counter app to ensure all elements are working properly

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,45 @@
 // import necessary react testing library helpers here
+import { render, fireEvent, screen } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter';
 
-beforeEach(() => {
-  // Render the Counter component here
-})
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
+  const counterMessage = screen.getByText(/Counter/);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const { getByTestId } = render(<Counter />);
+  const initialValue = Number(getByTestId("count").textContent);
+  expect(initialValue).toEqual(0);
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const { getByTestId, getByRole } = render(<Counter />);
+  const incrementBtn = getByRole("button", {name: "+"});
+
+  //now click the button
+  fireEvent.click(incrementBtn);
+  
+  //now test to see that our count value incremented
+  const countValue = Number(getByTestId("count").textContent);
+  expect(countValue).toEqual(1);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const { getByTestId, getByRole } = render(<Counter />);
+  const incrementBtn = getByRole("button", {name: "-"});
+
+  //now click the button
+  fireEvent.click(incrementBtn);
+  
+  //now test to see that our count value incremented
+  const countValue = Number(getByTestId("count").textContent);
+  expect(countValue).toEqual(-1);
 });
